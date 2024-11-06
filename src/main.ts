e7bda7cf550e7b837480758a3f9f41c885bd2dbf
deroldan26 <der2600@gmail.com>
2024-11-05 20:45:32 -0400

    feat: :sparkles: Swagger&Product
    InitialCommit with Swagger and Product implementation

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    }),
+  );
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Range',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Go Dely API')
+    .setDescription('Delivery app backend done with DDD.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(3000);
+}
+bootstrap();
