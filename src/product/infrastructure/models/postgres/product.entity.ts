d58b4dbe6bc6eb15fdf04a0d77b2a71cb8005e95
deroldan26 <der2600@gmail.com>
2024-11-07 23:26:34 -0400

    fix: :wrench: fixedProvider
    fixed product data provider

---
+++ src/product/infrastructure/models/postgres/product.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryColumn } from "typeorm"
+
+@Entity('Product')
+export class Product {
+    @PrimaryColumn(
+        'uuid',
+    )
+    id: string
+    // @Column({
+    //     type: 'varchar',
+    // })
+    // description: string
+    // @Column({
+    //     type: 'varchar',
+    // })
+    // weight: string
+    // @Column({
+    //     type: 'integer',
+    // })
+    // price: number
+    // @Column({
+    //     type: 'varchar',
+    // })
+    // stock: string
+    // @Column({
+    //     type: 'varchar',
+    // })
+    // currency: string
+    // @Column({
+    //     type: 'uuid',
+    // })
+    // image: string
+    @Column(
+        'varchar',
+    )
+    name: string
+}
